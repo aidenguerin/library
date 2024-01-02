efde5971cc2eaba3d130e38c9ba112bc70167d3d
Aiden Guerin <90363998+aidenguerin@users.noreply.github.com>
2024-01-02 21:49:16 +1100

    added book removal logic

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let book_id = 0;
-const myLibrary = [];
+let myLibrary = [];
 const booksGrid = document.querySelector(".books-grid");
 
 const dialog = document.querySelector("dialog");
@@ -20,7 +20,7 @@ addBookForm.addEventListener('submit', function(e) {
 
     const id = book_id;
     book_id += 1;
-    
+
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
@@ -52,6 +52,16 @@ function addBookToLibrary(book) {
     bookCard.classList.add("card"); 
     bookCard.dataset.title = book.title;
 
+    const btnRemoveCard = document.createElement("button");
+    btnRemoveCard.innerHTML = "x";
+
+    btnRemoveCard.addEventListener('click', (e) => {
+        const card = e.target.parentNode;
+        const title = {...card.dataset}['title'];
+        removeBookFromLibrary(title);
+        card.remove();
+    })
+
     const cardTitle = document.createElement("h2");
     cardTitle.innerText = book.title;
 
@@ -61,11 +71,29 @@ function addBookToLibrary(book) {
     const cardPages = document.createElement("p");
     cardPages.innerText = book.pages;
 
-
+    bookCard.appendChild(btnRemoveCard);
     bookCard.appendChild(cardTitle);
     bookCard.appendChild(cardAuthor);
     bookCard.appendChild(cardPages);
-    
     booksGrid.appendChild(bookCard);
 }   
+
+function removeBookFromLibrary(title) {
+    // Remove from array
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].title == title) {
+            myLibrary = removeItemFromArray(myLibrary, i)
+        }
+    }
+}
+
+function removeItemFromArray(arr, index) {
+    const newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (i != index) {
+            newArray.push(arr[i])
+        }
+    }
+    return newArray
+  }
 
